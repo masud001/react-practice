895a2a4d771adcedc7e99a820fd313ef254f2dcb
masud001 <madhnagar@gmail.com>
2019-06-30 03:13:07 +0600

    this is leasson 25 i finish nicely

---
+++ src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+
+    render() { 
+        return (
+            <React.Fragment>
+                <h1>This is the counte    r components</h1>
+                <button>Increments</button>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter;
