cf266bf1e6b5f9d942474b3c059bf291f091546b
masud001 <madhnagar@gmail.com>
2019-07-02 01:43:07 +0600

    this is lesson 27. i am here to learning how to use attibute in react js dynamically. to learn that i uses here a image. i set a attribute image url and image alt text

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 0,
+        imgAltText: "image name",
+        imgUrl: "https://picsum.photos/200"
     }
     render() { 
         return (
             <React.Fragment>
+                <img src={this.state.imgUrl} alt={this.state.imgAltText}/>
                 <span className="mx-2 my-3">{this.fonrmatCount()}</span>
                 <button className="btn btn-primary">Increments</button>
             </React.Fragment>
