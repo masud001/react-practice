32ab7e504248d2f491a611f8ff27e24050f6d8c5
masud001 <madhnagar@gmail.com>
2019-07-07 16:07:17 +0600

    this is composing component... here i want to build component tree..

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,26 +2,41 @@ import React, { Component } from 'react'
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imgAltText: "image name",
-        imgUrl: "https://picsum.photos/200"
+        value: this.props.value
     }
     style = {
-        fontSize:30,
+        fontSize:18,
         textTransform:"uppercase"
     }
+    handelIncrement = () => {
+        this.setState(
+            {
+                count: this.state.value + 1
+            }
+        )
+    }
     render() { 
-        
+        console.log("props : ", this.props);
         return (
             <React.Fragment>
-                <img src={this.state.imgUrl} alt={this.state.imgAltText}/>
-                <span style={this.style} className="m-2 p-2 badge badge-primary">{this.fonrmatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increments</button>
+                
+                <span style={this.style} className={this.getBadgeClasses()}>{this.fonrmatCount()}</span>
+                <button 
+                style={this.style} 
+                className="btn btn-secondary btn-sm"
+                onClick={this.handelIncrement}>Increments</button> <br/>
+                
             </React.Fragment>
         );
     }
+    getBadgeClasses() {
+        let classes = "m-2 p-2 badge badge-";
+        classes += (this.state.value === 0 ? "warning" : "primary");
+        return classes;
+    }
+
     fonrmatCount(){
-        const {count} = this.state;
+        const {value: count} = this.state;
         return count === 0 ? <span>Zero</span> : count;
     }
 }
