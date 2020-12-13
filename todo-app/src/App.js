e014302e64c932bde54363d09fef80d2f4c89db4
masud001 <madhnagar@gmail.com>
2020-12-14 00:49:01 +0600

    added css for design and header

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <h1> Hello world </h1>
+       <header>
+      <h1>Ed's Todo List</h1>
+    </header>
       
     </div>
   );
