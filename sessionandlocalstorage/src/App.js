b8bbf2585f78c902f357e617f9a860968da009c7
masud001 <madhnagar@gmail.com>
2020-12-17 18:19:50 +0600

    save react data in local storage and session storage and cookies

---
+++ sessionandlocalstorage/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Apps from "./Components/Apps";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello Local storage and seassion.</h1>
+			<Apps />
+		</div>
+	);
+}
+
+export default App;
