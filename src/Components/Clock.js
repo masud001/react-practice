935e063fd22a93cbfa40d064043c3782b81557eb
masud001 <madhnagar@gmail.com>
2021-08-20 01:11:24 +0600

    button is separate components

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,49 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Clock extends Component {
-   
-        state = {date: new Date(),locale:"bn-BD"}
-   
-    componentDidMount() {
-        this.clearTime = setInterval(()=> this.tick(),1000)
-    }
+	state = { date: new Date(), locale: 'bn-BD' };
 
-    componentWillUnmount() {
-        clearInterval(this.clearTime)
-    }
+	componentDidMount() {
+		this.clearTime = setInterval(() => this.tick(), 1000);
+	}
 
-    handelClick(locale) {
-       
-        this.setState({
-            locale
-        })
-        console.log("this is clicked")
-    }
+	componentWillUnmount() {
+		clearInterval(this.clearTime);
+	}
 
-    tick() {
-        this.setState({
-                date:new Date()
-            })
-    }
+	handelClick = (locale) => {
+		this.setState({
+			locale,
+		});
+		console.log('this is clicked');
+	};
 
-   
+	tick() {
+		this.setState({
+			date: new Date(),
+		});
+	}
 
-
-    render() {
-        const { date,locale } = this.state
-        return (
-            <React.Fragment>
-                <div className="container">
-                    <h1>Hello <span className=" badge bg-danger">React </span></h1>
-                    <h1>Today's Date is :
-                        <span className=" badge bg-dark ">
-                            {date.toLocaleTimeString(locale)}
-                        </span>
-                    </h1>
-                  
+	render() {
+		const { date, locale } = this.state;
+		return (
+			<React.Fragment>
+				<div className='container'>
+					<h1>
+						Hello <span className=' badge bg-danger'>React </span>
+					</h1>
+					<h1>
+						Today's Date is :
+						<span className=' badge bg-dark '>{date.toLocaleTimeString(locale)}</span>
+					</h1>
 				</div>
-            </React.Fragment>
-        )
-    }
+			</React.Fragment>
+		);
+	}
 }
-
