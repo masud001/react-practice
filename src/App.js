08a70b010c079d9c1e570a5cde53ef0ebb717cb9
masud001 <madhnagar@gmail.com>
2021-09-04 16:19:16 +0600

    render props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import ClickCounter from './Components/hoc/ClickCounter';
-import HoverCounter from './Components/hoc/HoverCounter';
+import ClickCounter from './Components/renderProps/ClickCounter';
+import HoverCounter from './Components/renderProps/HoverCounter';
+import Counter from './Components/renderProps/Counter';
+import { User } from './Components/User';
 
 class App extends Component {
 	render() {
@@ -8,10 +10,24 @@ class App extends Component {
 			<React.Fragment>
 				<div className='App'>
 					<div className='container'>
-						<h1 className=' text-capitalize '>Higher-Order Components</h1>
+						<h1 className=' text-capitalize '>Higher-Order Components render props</h1>
 						<hr />
-						<ClickCounter />
-						<HoverCounter />
+						{/* <ClickCounter />
+						<HoverCounter /> */}
+						{/* <ClickCounter /> */}
+						{/* <HoverCounter /> */}
+						<hr />
+						{/* <User render={(isLogin) => (isLogin ? 'Masudur Rahman' : 'Guest User')} /> */}
+						<Counter>
+							{(count, counterIncrement) => (
+								<ClickCounter count={count} counterIncrement={counterIncrement} />
+							)}
+						</Counter>
+						<Counter>
+							{(count, counterIncrement) => (
+								<HoverCounter count={count} counterIncrement={counterIncrement} />
+							)}
+						</Counter>
 					</div>
 				</div>
 			</React.Fragment>
