9ce363fd2778ce770c945537ff33e34ed5d13a16
masud001 <madhnagar@gmail.com>
2022-02-21 04:03:10 +0600

    create multiple reducer for multiple data

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
-console.log('this is from js');
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const combineReducers = redux.combineReducers;
+
+// action
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICE_CERAM = 'BUY_ICE_CERAM';
+// action creators
+function buyCake() {
+	return {
+		type: BUY_CAKE,
+		info: 'this is first redux action',
+	};
+}
+
+function buyIceCream() {
+	return {
+		type: 'BUY_ICE_CERAM',
+		info: 'this is second redux action',
+	};
+}
+
+const initialCakeState = {
+	numOfCakes: 10,
+};
+const initialIceCreamState = {
+	numOfIceCreams: 20,
+};
+
+// (previousState,action)=>newState
+const reducerCake = (state = initialCakeState, action) => {
+	switch (action.type) {
+		case BUY_CAKE:
+			return {
+				...state,
+				numOfCakes: state.numOfCakes - 1,
+			};
+
+		default:
+			return state;
+	}
+};
+const reducerIceCream = (state = initialIceCreamState, action) => {
+	switch (action.type) {
+		case BUY_ICE_CERAM:
+			return {
+				...state,
+				numOfIceCreams: state.numOfIceCreams - 1,
+			};
+
+		default:
+			return state;
+	}
+};
+
+const rootReducers = combineReducers({
+	cake: reducerCake,
+	iceCream: reducerIceCream,
+});
+
+const store = createStore(rootReducers);
+
+console.log('initial state : ', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('update state:', store.getState()));
+
+// buy cake
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+// buy ice-cream
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+
+unsubscribe();
